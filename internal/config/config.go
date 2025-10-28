a9716cdf2054d19a8af5a950f680c585514cf9bd
Decapitator <root@localhost.local>
2025-10-28 19:26:15 +0400

    Fix Mongo

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,10 @@
+// internal/config/config.go
 package config
 
 import (
 	"context"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/rs/zerolog/log"
@@ -14,7 +16,6 @@ type Config struct {
 	Port     string
 	MongoURI string
 	DBName   string
-
 	// runtime
 	Mongo *mongo.Client
 	DB    *mongo.Database
@@ -23,7 +24,7 @@ type Config struct {
 func Load() Config {
 	return Config{
 		Port:     getenv("PORT", "8080"),
-		MongoURI: os.Getenv("MONGO_URI"),
+		MongoURI: strings.TrimSpace(os.Getenv("MONGO_URI")), // trim just in case
 		DBName:   getenv("DB_NAME", "app"),
 	}
 }
@@ -33,21 +34,34 @@ func InitMongo(ctx context.Context, cfg *Config) {
 		log.Warn().Msg("MONGO_URI not set; Mongo features will be disabled")
 		return
 	}
-	cl, err := mongo.Connect(ctx, options.Client().ApplyURI(cfg.MongoURI))
+
+	// Short, bounded timeouts for connect & ping
+	connectCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	opts := options.Client().
+		ApplyURI(cfg.MongoURI).
+		SetServerAPIOptions(options.ServerAPI(options.ServerAPIVersion1)). // fine for Atlas
+		SetAppName("go-api").
+		SetRetryWrites(true)
+
+	cl, err := mongo.Connect(connectCtx, opts)
 	if err != nil {
 		log.Error().Err(err).Msg("mongo connect failed")
 		return
 	}
-	pctx, cancel := context.WithTimeout(ctx, 5*time.Second)
-	defer cancel()
-	if err := cl.Ping(pctx, nil); err != nil {
+
+	pingCtx, cancelPing := context.WithTimeout(ctx, 5*time.Second)
+	defer cancelPing()
+	if err := cl.Ping(pingCtx, nil); err != nil {
 		log.Error().Err(err).Msg("mongo ping failed")
 		_ = cl.Disconnect(context.Background())
 		return
 	}
+
 	cfg.Mongo = cl
 	cfg.DB = cl.Database(cfg.DBName)
-	log.Info().Str("db", cfg.DBName).Msg("mongo connected")
+	log.Info().Str("db", cfg.DBName).Bool("has_mongo_uri", cfg.MongoURI != "").Msg("mongo connected")
 }
 
 func getenv(k, d string) string {
